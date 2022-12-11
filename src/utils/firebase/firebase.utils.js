16cb26f7ba892c5bbb84affc35ad38c71f0db404
Gustavo Specht <guspecht@hotmail.com>
2022-12-11 15:16:31 +0800

    feat: sing up component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,48 +1,58 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUahwqmKiv0aU4WWTDjrIi4wz9kjHisdg",
-    authDomain: "mate-clothing-db.firebaseapp.com",
-    projectId: "mate-clothing-db",
-    storageBucket: "mate-clothing-db.appspot.com",
-    messagingSenderId: "1075350070638",
-    appId: "1:1075350070638:web:ec09be30b783ed0d874d7f"
-  };
+  apiKey: "AIzaSyCUahwqmKiv0aU4WWTDjrIi4wz9kjHisdg",
+  authDomain: "mate-clothing-db.firebaseapp.com",
+  projectId: "mate-clothing-db",
+  storageBucket: "mate-clothing-db.appspot.com",
+  messagingSenderId: "1075350070638",
+  appId: "1:1075350070638:web:ec09be30b783ed0d874d7f"
+};
   
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-  const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-  googleProvider.setCustomParameters({
-    prompt: "select_account"
-  });
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
 
-  export const firebaseAuth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(firebaseAuth, googleProvider);
+export const firebaseAuth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(firebaseAuth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(firebaseAuth, googleProvider);
 
-  export const fireStoreDatabase = getFirestore();
+export const fireStoreDatabase = getFirestore();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(fireStoreDatabase, 'users', userAuth.uid);
-    const userSnapshot = await getDoc(userDocRef);
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if(!userAuth) return;
 
-    if(!userSnapshot.exists())
-    {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
+  const userDocRef = doc(fireStoreDatabase, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
 
-      try {
-        await setDoc(userDocRef, {
-            displayName,
-            email,
-            createdAt
-        });
-      } catch (error) {
-        console.log('error creating the user', error.message);
-      }
+  if(!userSnapshot.exists())
+  {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+          ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('Error creating the user', error.message);
     }
-
-    return userDocRef;
   }
+
+  return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return ;
+
+  return await createUserWithEmailAndPassword(firebaseAuth, email, password);
+};
